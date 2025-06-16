0d1a4366cdf35c26760bc036f84cb9a72db90cb3
BernardoTobon <bernardotobon1@gmail.com>
2025-06-16 12:53:51 -0500

    Refactor page structure and add new components for improved functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Dashboard from "@/components/Dashboard";
+import AddClient from "@/components/data/AddClient";
+import AddEmployee from "@/components/data/AddEmployee";
+import AddSupplier from "@/components/data/AddSupplier";
+import Cellar from "@/components/data/Cellar";
+import CellarList from "@/components/data/CellarList";
+import ClientList from "@/components/data/ClientList";
+import EmployeeList from "@/components/data/EmployeeList";
+import InvoiceForm from "@/components/data/InvoiceForm";
+import ProductList from "@/components/data/ProductList";
+import RegisterAccount from "@/components/data/RegisterAccount";
+import RegisterAccountList from "@/components/data/RegisterAccountList";
+import RegisterCompany from "@/components/data/RegisterCompany";
+import RegisterProduct from "@/components/data/RegisterProduct";
+import SupplierList from "@/components/data/SupplierList";
+import Dashboard from "@/components/Login/Dashboard";
 import Login from "@/components/Login/Login";
 import SignUp from "@/components/Login/SignUp";
 
 export default function Home() {
   return (
-    <Login />
+    <SupplierList />
   );
 }
