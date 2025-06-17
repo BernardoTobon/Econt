445bc44302a31860e6348ca1ce0570540f30c8a1
BernardoTobon <bernardotobon1@gmail.com>
2025-06-17 17:53:21 -0500

    Add PurchaseHistory and SalesHistory components for tracking purchase and sales data

---
+++ src/app/purchase-history/page.tsx
@@ -0,0 +1,5 @@
+import PurchaseHistory from "@/components/data/PurchaseHistory";
+
+export default function PurchaseHistoryPge() {
+    return <PurchaseHistory />;
+}
---
+++ src/app/sales-history/page.tsx
@@ -0,0 +1,5 @@
+import SalesHistory from "@/components/data/SalesHistory";
+
+export default function SalesHistoryPage() {
+    return <SalesHistory />;
+}
